651b8f13417ae83d41f7153f997d17a0f6102767
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-16 11:46:23 -0300

    add NEXT.JS to branch_2023.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class', 
   theme: {
