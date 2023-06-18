cd808b93f73a64f223bd6392afa44b5c1a3b1c9e
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-18 19:01:23 -0300

    updating page contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,3 @@
-import Overlay from '@/components/overlay/Overlay';
-import React from 'react';
-
 export default function contact() {
   return (
     <div className='h-screen flex justify-center items-center'>
