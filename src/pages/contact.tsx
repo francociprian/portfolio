fa8df791e61ced2e757dc9fa5943f2329290493b
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-18 19:05:06 -0300

    updating page contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,28 @@
+import { motion, AnimatePresence, MotionProps, AnimatePresenceProps } from "framer-motion";
+
 export default function contact() {
+  const presenceProps: AnimatePresenceProps = {
+    mode: 'wait',
+  };
+  const ease: number[] = [0.08, 0.82, 0.17, 1];
+  const enterLeftProps: MotionProps = {
+    initial: { opacity: 0, x: -800 },
+    animate: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.95, delay: 0.2, ease: ease },
+    },
+    exit: {
+      opacity: 0,
+      x: -800,
+      transition: { duration: 0.95, ease: ease },
+    },
+  };
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <h1 className='text-6xl font-Sohne'>Contacto</h1>
-    </div>
+    <AnimatePresence {...presenceProps}>
+      <div className='h-screen flex justify-center items-center'>
+        <motion.h1 {...enterLeftProps} className='text-6xl font-Sohne'>Contact</motion.h1>
+      </div>
+    </AnimatePresence>
   )
 }
