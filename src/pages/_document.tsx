292a0ced0c4d5eb15647cb417aafb4782aa01338
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-19 10:46:55 -0300

    updating theme colors _documents.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-white/80 dark:bg-black/50'>
         <Main />
         <NextScript />
       </body>
