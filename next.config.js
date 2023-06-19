38532e6cb7845c17518901ae59fcbaeec6272fe0
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-19 14:33:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['res.cloudinary.com', 'source.unsplash.com'],
+  },
 }
 
 module.exports = nextConfig
