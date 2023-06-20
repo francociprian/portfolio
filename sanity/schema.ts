9921a8d1f07e89f63d1ee8e715e7bdfdf8dc776d
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-19 21:23:54 -0300

    [Sanity.io] Add Projects Example Schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+import project from './schemas/project';
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [ project ],
 }
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,48 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'techStack',
+      title: 'Tech Stack',
+      type: 'string',
+    }),
+    defineField({
+      name: 'deploy',
+      title: 'Deploy',
+      type: 'string',
+    }),
+    defineField({
+      name: 'github',
+      title: 'GitHub',
+      type: 'string',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+})
