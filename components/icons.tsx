0686b8b89e029ba328642501b8164419941a07f6
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-20 23:24:32 -0300

    Update Icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -75,3 +75,17 @@ export function SunIcon() {
         </svg>
     );
 }
+export const ChevronUpIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+        </svg>
+    )
+}
+export const ChevronDownIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+    )
+}
