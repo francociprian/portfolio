28e7c5bbcfb7962d6fa6c5104d8df44679891e3e
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-21 01:45:38 -0300

    Fetch data from [Sanity.io]

---
+++ pages/api/project.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { client } from '@/sanity/lib/client';
+import { groq } from "next-sanity";
+
+export default async function project(
+  req: NextApiRequest, 
+  res: NextApiResponse
+  ) {
+    try {
+      const project = await loadData();
+
+    res.status(200).json({
+      project
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: 'Error getting project data'
+    });
+  }
+}
+
+export async function loadData() {
+  try {
+    const query = groq`
+      *[_type == "project"] | order(publishedAt asc) {
+        _id, 
+        title, 
+        slug,
+        techStack,
+        deploy,
+        github,
+        details,
+        publishedAt, 
+        description,
+        imageMain,
+        imageMain2,
+        imageMain3,
+        imageMain4,
+        imageMobile,
+        imageMobile2,
+        imageMobile3
+      }`;
+    const project = await client.fetch(query);
+    return {
+      project
+    };
+  } catch (error) {
+    throw new Error('Error getting project data')
+  }
+}
