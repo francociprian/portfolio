897b161111ddcef2565bfc62db3a6089e260dbbb
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-22 20:17:59 -0300

    Update page project

--- pages/project.tsx
+++ pages/project.tsx
@@ -3,54 +3,13 @@ import { motion, Variants } from "framer-motion";
 import { loadData } from "./api/project";
 import { GetServerSideProps } from "next";
 
-type ImageCloudinary = {
-  url: string;
-  resource_type: string;
-  duration: null;
-  secure_url: string;
-  access_mode: string;
-  access_control: any[];
-  created_by: {
-    id: string;
-    type: string;
-  };
-  metadata: any[];
-  format: string;
-  type: string;
-  public_id: string;
-  tags: any[];
-  bytes: number;
-  _version: number;
-  height: number;
-  created_at: String;
-  uploaded_by: {
-    id: string;
-    type: string;
-  };
-  _type: string;
-  version: number;
-  width: number;
-  _key: string;
-}
 type Project = {
   _id: number;
   title: string;
-  techStack: string;
-  deploy: string;
-  github: string;
-  details: string;
-  description: string;
   slug: {
     current: string,
     _type: string
-  },
-  imageMain: ImageCloudinary;
-  imageMain2?: ImageCloudinary;
-  imageMain3?: ImageCloudinary;
-  imageMain4?: ImageCloudinary;
-  imageMobile?: ImageCloudinary;
-  imageMobile2?: ImageCloudinary;
-  imageMobile3?: ImageCloudinary;
+  }
 };
 
 export default function projects({ project }: { project: Project[] }){
