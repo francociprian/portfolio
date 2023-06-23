7324b6cbb18b71ce51465288c794eda585abf431
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-22 23:03:00 -0300

    Update _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body className='bg-white/80 dark:bg-black/50'>
         <Main />
         <NextScript />
