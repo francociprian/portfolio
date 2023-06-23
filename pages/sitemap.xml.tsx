d88b38ca7e2d5c915388ca4ac21dd24ab019c594
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-22 23:00:03 -0300

    Add sitemap.xml

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,48 @@
+import { client } from "@/sanity/lib/client";
+import type { NextApiResponse } from 'next'
+
+const createSitemap = ( slugs: string[] ) : string => 
+`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${slugs
+          .map((slug) => {
+            return `
+                <url>
+                    <loc>${`https://www.francociprian.com.ar/${slug}`}</loc>
+                </url>
+            `;
+          })
+          .join('')}
+    </urlset>
+`;
+
+export async function getServerSideProps({res}: {res: NextApiResponse}) {
+  const postSlugsQuery = `
+  *[_type == "project" && defined(slug.current)][].slug.current
+  `; 
+  const allPosts = await client.fetch(postSlugsQuery);
+  const allPages = [
+    ...[
+      '',
+      'project',
+      'contact',
+    ],
+    ...allPosts.map((slug:string) => `project/${slug}`)
+  ];
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=1200, stale-while-revalidate=600'
+  );
+  res.write(createSitemap(allPages));
+  res.end();
+
+  return {
+    props: {}
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
