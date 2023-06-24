8e3050c4e1909d8e1d35a2ba5d634984169f092c
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-24 19:45:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: ['res.cloudinary.com', 'source.unsplash.com'],
+    domains: [ 
+      'res.cloudinary.com',
+      'source.unsplash.com'
+    ],
+    unoptimized: true,
   },
 }
 
