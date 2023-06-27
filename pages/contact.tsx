4959d8540b882316c61df38c75dcc3020dfce5e3
Franco Ciprian <franco_ciprian@hotmail.com>
2023-06-27 18:58:16 -0300

    Update contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,40 @@
 import { motion, AnimatePresence, MotionProps, AnimatePresenceProps } from "framer-motion";
+import { Mail, Linkedin, FileText } from 'lucide-react'
+
+const socialLinks = [
+  {
+    title: "Curriculum",
+    url: "https://drive.google.com/file/d/1oL7RCxzYAJOC0ggCC0E1y-TR2uQKiVEL/view?usp=sharing",
+    icon: <FileText />
+  },
+  {
+    title: "E-mail",
+    url: "mailto:franco_ciprian@hotmail.com",
+    icon: <Mail />
+  },
+  {
+    title: "LinkedIn",
+    url: "http://www.linkedin.com/in/franco-ciprian-387231222/",
+    icon: <Linkedin />
+  },
+  {
+    title: "GitHub",
+    url: "https://github.com/francociprian",
+    icon: <svg
+      className="h-6 w-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  },
+];
+
 
 export default function contact() {
   const presenceProps: AnimatePresenceProps = {
@@ -18,10 +54,40 @@ export default function contact() {
       transition: { duration: 0.95, ease: ease },
     },
   };
+  const enterLinkProps: MotionProps = {
+    initial: { opacity: 0, y: -800 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.95, delay: 0.2, ease: ease },
+    },
+    exit: {
+      opacity: 0,
+      y: -800,
+      transition: { duration: 0.95, ease: ease },
+    },
+  };
   return (
     <AnimatePresence {...presenceProps}>
-      <div className='h-screen flex justify-center items-center'>
-        <motion.h1 {...enterLeftProps} className='text-6xl font-Sohne'>Contact</motion.h1>
+      <div className='h-screen flex flex-col justify-evenly items-center '>
+        <motion.h1 {...enterLeftProps} 
+          className='text-center font-extrabold tracking-tighter text-5xl md:text-6xl lg:text-8xl text-black dark:text-white font-Sohne'
+        >
+          Contact me
+        </motion.h1>
+        <div className="grid md:grid-cols-8 max-w-7xl w-full mx-auto">
+          {socialLinks.map((l, i) => (
+            <motion.div {...enterLinkProps} key={i} className="col-span-2 flex gap-2 items-center justify-center">
+              <a 
+                className="flex items-center gap-2"
+                href={l.url}
+                target="_blank"
+              >
+                {l.icon}<span>{l.title}</span>
+              </a>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </AnimatePresence>
   )
